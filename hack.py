de80de3ba3f3843136f1f1da60521e2aa99d81ee
yaburov-vv <Yaburov.V.Va@sberbank.ru>
2019-03-27 23:46:50 +0300

    added english sentences

--- hack.py
+++ hack.py
@@ -10,37 +10,37 @@ wordList = [x.replace('\n', '') for x in wordList]
 wls = WordListStats(wordList)
 wls.print_count_groups()
 
-print("Выберите слово: ")
+print("Choose word: ")
 cands = wls.find_candidates()
 print_list_num(cands)
-num = input("Вариант ")
+num = input("Variant ")
 selected_word = cands[int(num)-1]
 print('-->', selected_word)
 group = wls.wordCountGroups[selected_word]
 print_tree(group)
-num = input("Кол-во повторений ")
+num = input("Count matches ")
 
 wls = WordListStats(group[int(num)])
 wls.print_tree()
 wls.print_count_groups()
 cands = wls.find_candidates()
 print_list_num(cands)
-num = input("Вариант ")
+num = input("Variant ")
 selected_word = cands[int(num)-1]
 print('-->', selected_word)
 group = wls.wordCountGroups[selected_word]
 print_tree(group)
-num = input("Кол-во повторений ")
+num = input("Count matches ")
 
 wls = WordListStats(group[int(num)])
 wls.print_tree()
 wls.print_count_groups()
 cands = wls.find_candidates()
 print_list_num(cands)
-num = input("Вариант ")
+num = input("Variant ")
 selected_word = cands[int(num)-1]
 print('-->', selected_word)
 group = wls.wordCountGroups[selected_word]
 print_tree(group)
-num = input("Кол-во повторений ")
+num = input("Count matches ")
 
